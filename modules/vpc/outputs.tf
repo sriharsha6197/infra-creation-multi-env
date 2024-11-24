60716b3b54cfb4990675e4c7e443de408ada5f35
Sriharsha6197 <m.sriharsha97@gmail.com>
2024-11-24 13:35:04 +0530

    lp

--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -2,8 +2,8 @@ output "vpc_id" {
   value = aws_vpc.main.id
 }
 output "public_subnets" {
-  value = values[aws_subnet.main].id
+  value = values(aws_subnet.main)[*].id
 }
 output "private_subnets" {
-  value = values[aws_subnet.private_subnets].id
+  value = values(aws_subnet.private_subnets)[*].id
 }
